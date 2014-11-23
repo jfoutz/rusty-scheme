dc05bb589a115f6fbd1755aabaa47435ed9d376e
Jason Foutz <jfoutz@gmail.com>
2014-11-23 15:32:46 -0700

    improving returns

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,86 @@
 use std::str::Chars;
+use std::tuple::Tuple2;
 
-trait Parser<'a> {
-    fn m(&self, &mut Chars) -> bool;
+trait Parser<'a, T:Clone> {
+    fn m(&self, &mut Chars) -> (bool,T);
 }
 
-struct Match<'a> {
-    c : char
+struct Match<'a, T:Clone> {
+    c : char,
+    pass: T,
+    fail: T
+        
 }
 
-impl<'a> Parser<'a> for Match<'a> {
-    fn m(&self, src:&mut Chars) -> bool{
-        println!("match {} ", self.c);
+impl<'a, T:Clone> Parser<'a, T> for Match<'a, T> {
+    fn m(&self, src:&mut Chars) -> (bool,T){
+
         match src.next(){
-            Some(v) => v == self.c,
-            None => false
+            Some(v) => if v == self.c {
+                (true,self.pass.clone())
+            } else {
+                (false,self.fail.clone())
+            },
+            None => (false,self.fail.clone())
         }
     }
 }
 
-struct Or<'a> {
-    f: &'a Parser<'a> + 'a,
-    s: &'a Parser<'a> + 'a
+struct Or<'a,T:Clone> {
+    f: &'a Parser<'a,T> + 'a,
+    s: &'a Parser<'a,T> + 'a
 }
 
-impl<'a> Parser<'a> for Or<'a> {
-    fn m(&self, src:&mut Chars) -> bool{
-        println!("or ");
-        let mut save = src.clone();
-        if self.f.m(src){
-            true
+impl<'a,T:Clone> Parser<'a,T> for Or<'a,T> {
+    fn m(&self, src:&mut Chars) -> (bool,T){
+
+        let save = src.clone();
+        let (x,t) = self.f.m(src);
+        if x {
+            (x,t)
         } else {
-            self.s.m(&mut save)
-        }
-    }
-}
-
-struct And<'a> {
-    f: &'a Parser<'a> + 'a,
-    s: &'a Parser<'a> + 'a
-}
-
-impl<'a> Parser<'a> for And<'a> {
-    fn m(&self, src:&mut Chars) -> bool{
-        println!("and ");
-        if self.f.m(src){
+            *src = save;
             self.s.m(src)
+        }
+    }
+}
+
+struct And<'a,T:Clone> {
+    f: &'a Parser<'a,T> + 'a,
+    s: &'a Parser<'a,T> + 'a
+}
+
+impl<'a,T:Clone> Parser<'a,T> for And<'a,T> {
+    fn m(&self, src:&mut Chars) -> (bool,T){
+        let save = src.clone();
+        let (x,t) = self.f.m(src);
+        if x {
+            let (x2,t2) = self.s.m(src);
+            if x2 {
+                (x2,t2)
+            } else {
+                *src = save;
+                (x2,t2)
+            }
         } else {
-            false
+            *src = save;
+            (x,t)
         }
     }
 }
 
 
-fn foo(p:&Parser){
-    println!("ac {}", p.m(&mut ("ac".chars())));
-    println!("bc {}", p.m(&mut ("bc".chars())));
+fn foo<'a,T:Clone>(p:&Parser<'a,T>){
+    println!("ac {}", (p.m(&mut ("ac".chars()))).val0());
+    println!("bc {}", (p.m(&mut ("bc".chars()))).val0());
 }
 
 fn main(){
     let o = And{
-        f:&Or{f:&Match{c:'a'},
-             s:&Match{c:'b'}},
-        s:&Match{c:'c'}
+        f:&Or{f:&Match{c:'a',pass:true,fail:false},
+             s:&Match{c:'b',pass:true,fail:false}},
+        s:&Match{c:'c',pass:true,fail:false}
     };
-    let c = & mut("abc".chars());
-    o.m(c);
 
-    o.m(&mut ("def".chars()));
     foo(&o);
 }
